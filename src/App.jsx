d974e7a9b1b96a7876259bb8c321622f0fd0eb9c
aezizhu <aezi.zhu@icloud.com>
2025-04-20 15:00:15 -0400

    Add src/App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from './styles/GlobalStyle';
+import theme from './styles/theme';
+import { AudioProvider } from './contexts/AudioContext';
+import { SettingsProvider } from './contexts/SettingsContext';
+import { AppContainer, MainContent, Content } from './styles/Layout';
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
+import Footer from './components/Footer';
+import SettingsModal from './components/SettingsModal';
+import Home from './pages/Home';
+
+// Import game components
+import Game2048 from './games/Game2048';
+const Tetris = () => <div>Tetris Game (Coming Soon)</div>;
+const Snake = () => <div>Snake Game (Coming Soon)</div>;
+const SlotMachine = () => <div>Slot Machine Game (Coming Soon)</div>;
+const Points24 = () => <div>24 Points Game (Coming Soon)</div>;
+const Doudizhu = () => <div>Doudizhu Game (Coming Soon)</div>;
+const Help = () => <div>Help & Instructions (Coming Soon)</div>;
+
+// Main App component
+function App() {
+  return (
+    <Router>
+      <ThemeProvider theme={theme}>
+        <AudioProvider>
+          <SettingsProvider>
+            <GlobalStyle />
+            <AppContainer>
+              <Header />
+              <MainContent>
+                <Sidebar />
+                <Content>
+                  <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/2048" element={<Game2048 />} />
+                    <Route path="/tetris" element={<Tetris />} />
+                    <Route path="/snake" element={<Snake />} />
+                    <Route path="/slot-machine" element={<SlotMachine />} />
+                    <Route path="/24-points" element={<Points24 />} />
+                    <Route path="/doudizhu" element={<Doudizhu />} />
+                    <Route path="/help" element={<Help />} />
+                  </Routes>
+                </Content>
+              </MainContent>
+              <Footer />
+              <SettingsModal />
+            </AppContainer>
+          </SettingsProvider>
+        </AudioProvider>
+      </ThemeProvider>
+    </Router>
+  );
+}
+
+export default App;
